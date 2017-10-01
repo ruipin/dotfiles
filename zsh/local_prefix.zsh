d97f65f38c6f41eea5f5ef38f0128b46beb1375e
ruipgpinheiro <rui@ruipinheiro.eu>
2017-10-01 22:21:36 +0100

    zsh: Add all necessary paths for local prefix to work correctly

--- zsh/local_prefix.zsh
+++ zsh/local_prefix.zsh
@@ -10,8 +10,26 @@ if [[ -z "$LOCAL_PREFIX" ]]; then
 	return
 fi
 
-
+###############
 # Set up local prefix
-# TODO: Make sure we have all the environment variables
-env_prepend PATH "$LOCAL_PREFIX/bin"
-env_prepend LD_LIBRARY_PATH "$LOCAL_PREFIX/lib"
+
+# Add local usr folder to path
+env_prepend PATH "${LOCAL_PREFIX}/bin"
+
+# Add local lib folder to library path
+env_prepend LD_LIBRARY_PATH "${LOCAL_PREFIX}/lib64:${LOCAL_PREFIX}/lib"
+env_prepend LIBRARY_PATH "${LOCAL_PREFIX}/lib64:${LOCAL_PREFIX}/lib"
+
+env_append PKG_CONFIG_PATH "${LOCAL_PREFIX}/lib/pkgconfig"
+
+# Add local include path to CPATH
+env_prepend CPATH "${LOCAL_PREFIX}/include"
+env_prepend C_INCLUDE_PATH "${LOCAL_PREFIX}/include"
+env_prepend CPLUS_INCLUDE_PATH "${LOCAL_PREFIX}/include"
+
+# Add local docs to man paths
+env_prepend MANPATH "${LOCAL_PREFIX}/share/man"
+env_prepend INFOPATH "${LOCAL_PREFIX}/share/man"
+
+# LUA override
+#export LUA_PREFIX="${LOCAL_PREFIX}"

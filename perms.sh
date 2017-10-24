b28d1612d60da5c1956ec1b5180f9127fcf2483f
Rui Pinheiro <rui@ruipinheiro.eu>
2017-10-24 14:58:38 +0100

    Add script for setting up repo file permissions

---
+++ perms.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Removes group/others permissions from CWD
+# Useful to make sure that others in the same machine (non-root) cannot access dotfiles folder
+
+find . -type d -exec chmod u+rwx,g-rwx,o-rwx {} \;
+find . -type f -exec chmod u+rw,g-rwx,o-rwx {} \;

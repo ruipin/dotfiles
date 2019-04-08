6aab726bf8214b25ba9eb7423429fac22123a9cc
Rui Pinheiro <rui@ruipinheiro.eu>
2019-04-08 12:38:12 +0100

    Add libtermkey build script

---
+++ update/libtermkey.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Init
+source "$(dirname $(readlink -f $0))/lib_update.bash"
+clone "libtermkey" --github "neovim/libtermkey" --ver "master" $@
+
+# Build
+make PREFIX=${LOCAL_PREFIX}
+make install PREFIX=${LOCAL_PREFIX}

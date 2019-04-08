d27c235ce90dda04ea3112866d383187d3b3eeb9
Rui Pinheiro <rui@ruipinheiro.eu>
2019-04-08 12:31:24 +0100

    Add libevent build script

---
+++ update/libevent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Init
+source "$(dirname $(readlink -f $0))/lib_update.bash"
+clone "libevent" --github "libevent/libevent" --ver "release-2.1.8-stable.tar.gz" $@
+
+# Build
+./autogen.sh
+./configure --prefix=$LOCAL_PREFIX
+make
+make install

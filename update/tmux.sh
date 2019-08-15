2631c54f03595526651f1c93789a3916c491d68f
Rui Pinheiro <rui@ruipinheiro.eu>
2019-08-15 17:58:27 +0100

    tmux: update to 2.9a

--- update/tmux.sh
+++ update/tmux.sh
@@ -2,7 +2,7 @@
 
 # Init
 source "$(dirname $(readlink -f $0))/lib_update.bash"
-clone "tmux" --github "tmux/tmux" --ver "2.8.tar.gz" $@
+clone "tmux" --github "tmux/tmux" --ver "2.9a.tar.gz" $@
 
 
 # Disable reflow functionality

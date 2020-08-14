40d3f8082479cc402e67254bf136cb9d0024366a
Rui Pinheiro <rui@ruipinheiro.eu>
2020-08-14 11:10:28 +0100

    Downgrade libxz, add sed build script

---
+++ update/sed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Init
+source "$(dirname $(readlink -f $0))/lib_update.bash"
+clone "sed" -t "http://ftp.gnu.org/gnu/sed/sed-4.8.tar.gz" "$@"
+
+
+# Build
+./configure --prefix="${LOCAL_PREFIX}"
+make
+make install

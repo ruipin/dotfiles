1bd37ab2970258378422e98919d4949b012c6668
Rui Pinheiro <rui@ruipinheiro.eu>
2023-07-17 01:23:01 +0100

    ansible: Add ansible-pull playbook

---
+++ ansible/files/install_zplug.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# We need to patch zplug to run properly without an interactive shell
+# Otherwise it believes we are trying to pipe in a command
+sed -i 's~\[\[ -p /dev/stdin ]]~false~' ~/.zplug/base/core/add.zsh
+
+# Prepare environment
+export ZPLUG_ROOT=~/.zplug
+export ZSHRC_ZPLUG_INIT_TYPE=3
+source ~/dotfiles/zsh/zshrc.d/10-env/00-script_utils
+
+# Call up zplug
+source ~/dotfiles/zsh/zshrc.d/70-zplug > ~/.zplug/ansible.log
+[[ "$?" -ne "0" ]] && exit 1
+
+# Parse output to decide whether something changed
+grep -q 'Installed' ~/.zplug/ansible.log && echo "Installed"
+
+# Done
+exit 0

18d3441e1cf2e4ff0d361720bfbc13b000aec270
Rui Pinheiro <rui@ruipinheiro.eu>
2024-03-05 18:31:00 +0000

    Tweaks for FreeBSD compatibility

--- ansible/files/install_zplug.sh
+++ ansible/files/install_zplug.sh
@@ -1,8 +1,8 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 # We need to patch zplug to run properly without an interactive shell
 # Otherwise it believes we are trying to pipe in a command
-sed -i 's~\[\[ -p /dev/stdin ]]~false~' ~/.zplug/base/core/add.zsh
+sed -i '' -e 's~\[\[ -p /dev/stdin ]]~false~' ~/.zplug/base/core/add.zsh
 
 # Prepare environment
 export ZPLUG_ROOT=~/.zplug
@@ -11,7 +11,7 @@ source ~/dotfiles/zsh/zshrc.d/10-env/00-script_utils
 
 # Call up zplug
 source ~/dotfiles/zsh/zshrc.d/70-zplug > ~/.zplug/ansible.log
-[[ "$?" -ne "0" ]] && exit 1
+[[ "$?" -ne "0" ]] && exit1
 
 # Parse output to decide whether something changed
 grep -q 'Installed' ~/.zplug/ansible.log && echo "Installed"
